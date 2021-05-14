1d11fbdac809bb0b684f2e14ecc6fc9699d491e7
Robert <rob.l.murphy92@gmail.com>
2021-05-14 16:16:26 -0500

    added in prepare func for iris data

---
+++ prepare.py
@@ -0,0 +1,12 @@
+def prep_iris(df):
+    '''
+    takes in a df of the iris dataset as it is acquired and returns a cleaned df
+    arguements: df: a pandas df with the expected feature names and columns return: 
+    clean_df: a dataframe with the cleaning operations performed on it
+    '''
+    df = df.drop_duplicates()
+    df = df.drop(columns = ['species_id'])
+    df = df.rename(columns={"species_name": "species"})
+    dummy_species_name = pd.get_dummies(df[['species']]) 
+    return df
+    
