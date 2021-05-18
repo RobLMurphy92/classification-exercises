e2e5818d7dda3fd0da6b592a648f1079e8448f84
Robert <rob.l.murphy92@gmail.com>
2021-05-18 12:29:50 -0500

    updated

--- prepare.py
+++ prepare.py
@@ -20,15 +20,5 @@ def prep_iris(df):
     return df
 
 
-def train_validate_test_split(df, target, seed=123):
-    train_and_validate, test = train_test_split(
-        df, test_size=0.2, random_state=seed, stratify=df.survived
-    )
-    train, validate = train_test_split(
-        train_and_validate,
-        test_size=0.3,
-        random_state=seed,
-        stratify=train_and_validate.survived,
-    )
-    return train, validate, test
+
     
