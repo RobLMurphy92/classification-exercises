b80ce6c02767982109c496f8b064d731cd49190f
Robert <rob.l.murphy92@gmail.com>
2021-05-18 12:02:09 -0500

    corrected prepar

--- prepare.py
+++ prepare.py
@@ -13,5 +13,19 @@ def prep_iris(df):
     df = df.drop(columns = ['species_id'])
     df = df.rename(columns={"species_name": "species"})
     dummy_species_name = pd.get_dummies(df[['species']]) 
+    df = pd.concat([df, dummy_species_name], axis=1)
     return df
+
+
+    def train_validate_test_split(df, seed=123):
+    train_and_validate, test = train_test_split(
+        df, test_size=0.2, random_state=seed, stratify=df.survived
+    )
+    train, validate = train_test_split(
+        train_and_validate,
+        test_size=0.3,
+        random_state=seed,
+        stratify=train_and_validate.survived,
+    )
+    return train, validate, test
     
